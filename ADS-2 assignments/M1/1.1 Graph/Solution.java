c7c384d509c9c9aab21050f59165fdba31b1300f
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-29 19:25:19 +0530

    "ads2_m1_a1_1: 4/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/M1/1.1 Graph/Solution.java
+++ ADS-2 assignments/M1/1.1 Graph/Solution.java
@@ -7,13 +7,16 @@ interface Graph {
     public Iterable<Integer> adj(int v);
     public boolean hasEdge(int v, int w);
 }
-class AdjacencyList implements Graph {
-    private final int V;
+class GraphADT implements Graph {
+    private int V;
     private int E;
-    private Bag<Integer>[] adj; 
-    public AdjacencyList(int V, int E) {
+    private Bag<Integer>[] adj;
+    GraphADT() {
+        //empty constructor.
+    }
+    public GraphADT(int V) {
         this.V = V;
-        this.E = E;
+        this.E = 0;
         adj = (Bag<Integer>[]) new Bag[V];
         for (int v = 0; v < V; v++) {
             adj[v] = new Bag<Integer>();
@@ -40,23 +43,51 @@ class AdjacencyList implements Graph {
         }
         return false;
     }
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        s.append(V + " vertices, " + E + " edges " + "\n");
-        for (int v = 0; v < V; v++) {
-            s.append(v + ": ");
-            for (int w : adj[v]) {
-                s.append(w + " ");
+    public void listdisplay(int v, int w, String[] tokens) throws Exception {
+        if (w <= 1 && v <= 1) {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            throw new Exception("No edges");
+        } else {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            for (int i = 0; i < tokens.length; i++) {
+            String str = "";
+            str = tokens[i] + ": ";
+            for (int k : adj(i)) {
+                str = str + tokens[k] + " ";
             }
-            s.append("\n");
+            System.out.println(str);
+            }
         }
-        return s.toString();
+    }
+     public void matrixdisplay(int v, int w) throws Exception {
+        if (w <= 1 && v <= 1) {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            throw new Exception("No edges");
+        } else {
+            System.out.println(V() + " vertices" + ", " + E() + " edges");
+            int[][] disp = new int[v][v];
+            for (int i = 0; i  < v; i++) {
+                for (int j = 0; j < v; j++) {
+                    if (hasEdge(i, j)) {
+                        disp[i][j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < v; i++) {
+                for (int j = 0; j < v; j++) {
+                    System.out.print(disp[i][j] + " ");
+                }
+                System.out.println();
+            }
+            
+        }
     }
 }
 // class AdjacencyMatrix implements Graph {
 //     private final int V;
 //     private int E;
-//     private int[][] matrix; 
+//     private boolean[][] matrix;
 //     public AdjacencyMatrix(int V) {
 //         this.V = V;
 //         this.E = 0;
@@ -70,7 +101,7 @@ class AdjacencyList implements Graph {
 //     }
 //     public int E() {
 //         return E;
-//     } 
+//     }
 
 /**
  * class for Solution.
@@ -89,25 +120,34 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
-        String type = scan.nextLine();
-        int vertices = Integer.parseInt(scan.nextLine());
-        int edges = Integer.parseInt(scan.nextLine());
-        if (edges == 0) {
-            System.out.println("No edges");
+        GraphADT gobj = new GraphADT();
+        String input = scan.nextLine();
+        int v = Integer.parseInt(scan.nextLine());
+        int e = Integer.parseInt(scan.nextLine());
+        String keynames = scan.nextLine();
+        String[] tokens = keynames.split(",");
+        gobj = new GraphADT(v);
+        while(scan.hasNext()) {
+            String connect = scan.nextLine();
+            String[] connections = connect.split(" ");
+            gobj.addEdge(Integer.parseInt(connections[0]),
+                Integer.parseInt(connections[1]));
         }
-        String[] names = scan.nextLine().split(",");
-        // System.out.println(Arrays.toString(names));
-        while (edges > 0) {
-            String[] connections = scan.nextLine().split(" ");
-            switch (type) {
+        switch (input) {
             case "List":
-             AdjacencyList al = new AdjacencyList(vertices, edges);
-             System.out.println(al);
-            // case "Matrix":
-            //  AdjacencyMatrix am = new AdjacencyMatrix();
-            // }
-                // System.out.println(Arrays.toString(connections));
+            try {
+                gobj.listdisplay(v, e, tokens);
+            } catch (Exception p) {
+                System.out.println(p.getMessage());
             }
+            break;
+            case "Matrix":
+            try {
+                gobj.matrixdisplay(v, e);
+            } catch (Exception p) {
+                System.out.println(p.getMessage());
+            }
+            break;
         }
     }
 }
