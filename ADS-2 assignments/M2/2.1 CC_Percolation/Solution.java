c0b155cb570b59cceea27a011ee76dd06893ba6e
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-10-30 13:41:41 +0530

    "ads2_m2_a1_1: 6/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 assignments/M2/2.1 CC_Percolation/Solution.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+/**
+ * Class for graph.
+ */
+class Graph {
+    private int[][] grid;
+    private int vertices;
+    private int edges;
+    Graph(final int vertices) {
+        grid = new int[vertices][vertices];
+    }
+    public int vertices() {
+        return vertices;
+    }
+    public void addEdge(final int vertexOne, final int vertexTwo) {
+        if (vertexOne != vertexTwo) {
+            if (!hasEdge(vertexOne, vertexTwo)) {
+                grid[vertexOne][vertexTwo] = 1;
+                grid[vertexTwo][vertexOne] = 1;
+                edges++;
+            }
+        }
+    }
+    public boolean hasEdge(final int vertexOne, final int vertexTwo) {
+        if (grid[vertexOne][vertexTwo] == 1) {
+            return true;
+        }
+        return false;
+    }
+    // public void connected(final int v1, final int w1) {
+    //     grid[v1][w1] = 1;
+    // }
+    public int[] adj(final int v) {
+        return grid[v];
+    }
+}
+class ConnectedComponents {
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+    ConnectedComponents(Graph g, int s) {
+        marked = new boolean[g.vertices()];
+        id  = new int[g.vertices()];
+        for (int i = 0; i < g.vertices(); i++) {
+            if (!marked[i]) {
+                dfs(g, i);
+                count++;
+            }
+        }
+    }
+    public int count() {
+        return count;
+    }
+    public int id(int v) {
+        return id[v];
+    }
+    private void dfs(Graph g, int v) {
+        marked[v] = true;
+        id[v] = count;
+        for (int each : g.adj(v)) {
+            if (!marked[each]) {
+                dfs(g, each);
+            }
+        }
+    }
+    public boolean percolates() {
+        if (count == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+public final class Solution {
+    private Solution() {
+    }
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String num = scan.nextLine();
+        Graph gobj = new Graph(Integer.parseInt(num));
+        while (scan.hasNext()) {
+            String line = scan.nextLine();
+            String[] tokens = line.split(" ");
+            gobj.addEdge(Integer.parseInt(tokens[0]) - 1, Integer.parseInt(tokens[1]) - 1);
+        }
+        ConnectedComponents obj = new ConnectedComponents(gobj, Integer.parseInt(num));
+        System.out.println(obj.percolates());
+    }
+}
