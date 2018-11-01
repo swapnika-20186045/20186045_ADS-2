40f6ab228388314f9ebc0df06cebdbe65f0387a0
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-11-01 23:12:52 +0530

    "ads2_m4_a1_1: 3/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/M4/Code Camp - WordNet/WordNet/WordNet.java
+++ ADS-2 assignments/M4/Code Camp - WordNet/WordNet/WordNet.java
@@ -10,61 +10,64 @@ public class WordNet {
     public WordNet(final String synsets, final String hypernyms) {
         nounST = new LinearProbingHashST<String, ArrayList<Integer>>();
         idST = new LinearProbingHashST<Integer, String>();
+        try {
+            In in = new In("./Files/" + synsets);
+            int id = 0;
+            while (!in.isEmpty()) {
 
-        In in = new In("./Files/" + synsets);
-        int id = 0;
-        while (!in.isEmpty()) {
+                String line = in.readLine();
+                
+                assert !line.equals("");
+                
+                String[] tokens = line.split(",");
+                id = Integer.parseInt(tokens[0]);
+                String[] nouns = tokens[1].split(" ");
 
-            String line = in.readLine();
-            
-            assert !line.equals("");
-            
-            String[] tokens = line.split(",");
-            id = Integer.parseInt(tokens[0]);
-            String[] nouns = tokens[1].split(" ");
+                ArrayList<String> nounsList = new ArrayList<String>();
+                for (String noun : nouns) {
+                    nounsList.add(noun);
+                }
+                idST.put(id, tokens[1]);
 
-            ArrayList<String> nounsList = new ArrayList<String>();
-            for (String noun : nouns) {
-                nounsList.add(noun);
-            }
-            idST.put(id, tokens[1]);
-
-            for (String noun : nouns) {
-                if (nounST.contains(noun)) {
-                    nounST.get(noun).add(id);
-                } else {
-                    ArrayList<Integer> s = new ArrayList<Integer>();
-                    s.add(id);
-                    nounST.put(noun, s);
+                for (String noun : nouns) {
+                    if (nounST.contains(noun)) {
+                        nounST.get(noun).add(id);
+                    } else {
+                        ArrayList<Integer> s = new ArrayList<Integer>();
+                        s.add(id);
+                        nounST.put(noun, s);
+                    }
                 }
             }
-        }
 
-        //Hypernyms
-        assert id != 1;
-        this.dg = new Digraph(id + 1);
+            //Hypernyms
+            assert id != 1;
+            this.dg = new Digraph(id + 1);
 
-        in = new In("./Files/" + hypernyms);
-        while (!in.isEmpty()) {
-            String line = in.readLine();
-            String[] tokens = line.split(",");
-            // if (tokens.length > 2) {
-            //     throw new IllegalArgumentException("Multiple roots"); 
-            // }
-            int synsetIds = Integer.parseInt(tokens[0]);
+            in = new In("./Files/" + hypernyms);
+            while (!in.isEmpty()) {
+                String line = in.readLine();
+                String[] tokens = line.split(",");
+                // if (tokens.length > 2) {
+                //     throw new IllegalArgumentException("Multiple roots"); 
+                // }
+                int synsetIds = Integer.parseInt(tokens[0]);
 
-            for (int i = 1; i < tokens.length; i++) {
-                dg.addEdge(synsetIds, Integer.parseInt(tokens[i]));
+                for (int i = 1; i < tokens.length; i++) {
+                    dg.addEdge(synsetIds, Integer.parseInt(tokens[i]));
+                }
             }
-        }
 
-        DirectedCycle dc = new DirectedCycle(dg);
-        if (dc.hasCycle()) {
-            System.out.println("Cycle detected");
-        } else if (dg.maxOutdegree() > 1) {
-            System.out.println("Multiple roots");
-        } else {
-            System.out.println(dg.toString());
+            DirectedCycle dc = new DirectedCycle(dg);
+            if (dc.hasCycle()) {
+                System.out.println("Cycle detected");
+            } else if (dg.maxOutdegree() > 1) {
+                System.out.println("Multiple roots");
+            } else {
+                System.out.println(dg.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         //graph built
     }
