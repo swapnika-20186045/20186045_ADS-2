3f7d525c22295ea9cac71e6e3eac30ca29b904aa
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-11-01 23:00:10 +0530

    "ads2_m4_a1_1: 2/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 assignments/M4/Code Camp - WordNet/WordNet/Solution.java
@@ -0,0 +1,26 @@
+public final class Solution {
+	private Solution() {
+		//empty constructor.
+	}
+	public static void main(final String[] args) {
+		String synsetsFileName = StdIn.readString();
+		// In in = new In(str);
+		// System.out.println(strsynsetsFileName + "lol");
+		String hypernymsFileName = StdIn.readString();
+		// In in1 = new In(str1);
+		// System.out.println(hypernymsFileName + "lollll");
+		// System.out.println(wn.nouns());
+		String str2 = StdIn.readString();
+		switch (str2) {
+			case "Graph":
+				WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
+				System.out.println(wn.getDg().toString());
+				// System.out.println(wn.readHypernyms(str2));
+			break;
+		// 	case "Queries":
+		// 	break;
+			default:
+			break;
+		}
+	}
+}
---
+++ ADS-2 assignments/M4/Code Camp - WordNet/WordNet/WordNet.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+public class WordNet {
+    private LinearProbingHashST<String, ArrayList<Integer>> nounST;
+    private LinearProbingHashST<Integer, String> idST;
+    private Digraph dg;
+
+    public Digraph getDg() {
+        return this.dg;
+    }
+    public WordNet(final String synsets, final String hypernyms) {
+        nounST = new LinearProbingHashST<String, ArrayList<Integer>>();
+        idST = new LinearProbingHashST<Integer, String>();
+
+        In in = new In("./Files/" + synsets);
+        int id = 0;
+        while (!in.isEmpty()) {
+
+            String line = in.readLine();
+            
+            assert !line.equals("");
+            
+            String[] tokens = line.split(",");
+            id = Integer.parseInt(tokens[0]);
+            String[] nouns = tokens[1].split(" ");
+
+            ArrayList<String> nounsList = new ArrayList<String>();
+            for (String noun : nouns) {
+                nounsList.add(noun);
+            }
+            idST.put(id, tokens[1]);
+
+            for (String noun : nouns) {
+                if (nounST.contains(noun)) {
+                    nounST.get(noun).add(id);
+                } else {
+                    ArrayList<Integer> s = new ArrayList<Integer>();
+                    s.add(id);
+                    nounST.put(noun, s);
+                }
+            }
+        }
+
+        //Hypernyms
+        assert id != 1;
+        this.dg = new Digraph(id + 1);
+
+        in = new In("./Files/" + hypernyms);
+        while (!in.isEmpty()) {
+            String line = in.readLine();
+            String[] tokens = line.split(",");
+            // if (tokens.length > 2) {
+            //     throw new IllegalArgumentException("Multiple roots"); 
+            // }
+            int synsetIds = Integer.parseInt(tokens[0]);
+
+            for (int i = 1; i < tokens.length; i++) {
+                dg.addEdge(synsetIds, Integer.parseInt(tokens[i]));
+            }
+        }
+
+        DirectedCycle dc = new DirectedCycle(dg);
+        if (dc.hasCycle()) {
+            throw new IllegalArgumentException("Multiple roots");
+        }
+        //graph built
+    }
+    public Iterable<String> nouns() {
+        return nounST.keys();
+    }
+
+    public boolean isNoun(final String word) {
+        return nounST.contains(word);
+    }
+
+}
