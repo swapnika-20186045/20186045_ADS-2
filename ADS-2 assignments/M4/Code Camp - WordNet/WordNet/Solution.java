e543db9cfb3ecc5919f39aae9a453ec24853ff6e
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-11-02 00:29:18 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/M4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2 assignments/M4/Code Camp - WordNet/WordNet/Solution.java
@@ -1,42 +1,42 @@
 public final class Solution {
-	private Solution() {
-		//empty constructor.
-	}
-	public static void main(final String[] args) {
-		String synsetsFileName = StdIn.readLine();
-		// In in = new In(str);
-		// System.out.println(strsynsetsFileName + "lol");
-		String hypernymsFileName = StdIn.readLine();
-		// In in1 = new In(str1);
-		// System.out.println(hypernymsFileName + "lollll");
-		// System.out.println(wn.nouns());
-		String str2 = StdIn.readLine();
-		switch (str2) {
-			// WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
-			case "Graph":
-				// System.out.println(wn.getDg().toString());
-				WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
-				wn.display();
-			break;
-			case "Queries":
-			// System.out.println("lll");
-				try {
-					WordNet wnq = new WordNet(synsetsFileName, hypernymsFileName);
-					// System.out.println(line + "ooo");
-					while (StdIn.hasNextLine()) {
-						String line = StdIn.readLine();
-						String[] strarr = line.split(" ");
-						if (strarr[0].equals("null")) {
-							System.out.println("IllegalArgumentException");
-						}
-						System.out.println("distance = " + wnq.distance(strarr[0], strarr[1]) + ", ancestor = " + wnq.sap(strarr[0], strarr[1]));
-					}
-				} catch (Exception e) {
-            		System.out.println(e.getMessage());
-        		}
-			break;
-			default:
-			break;
-		}
-	}
+    private Solution() {
+        //empty constructor.
+    }
+    public static void main(final String[] args) {
+        String synsetsFileName = StdIn.readLine();
+        // In in = new In(str);
+        // System.out.println(strsynsetsFileName + "lol");
+        String hypernymsFileName = StdIn.readLine();
+        // In in1 = new In(str1);
+        // System.out.println(hypernymsFileName + "lollll");
+        // System.out.println(wn.nouns());
+        String str2 = StdIn.readLine();
+        switch (str2) {
+            // WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
+            case "Graph":
+                // System.out.println(wn.getDg().toString());
+                WordNet wn = new WordNet(synsetsFileName, hypernymsFileName);
+                wn.display();
+            break;
+            case "Queries":
+            // System.out.println("lll");
+                try {
+                    WordNet wnq = new WordNet(synsetsFileName, hypernymsFileName);
+                    // System.out.println(line + "ooo");
+                    while (StdIn.hasNextLine()) {
+                        String line = StdIn.readLine();
+                        String[] strarr = line.split(" ");
+                        if (strarr[0].equals("null")) {
+                            throw new IllegalArgumentException("IllegalArgumentException");
+                        }
+                        System.out.println("distance = " + wnq.distance(strarr[0], strarr[1]) + ", ancestor = " + wnq.sap(strarr[0], strarr[1]));
+                    }
+                } catch (Exception e) {
+                    System.out.println("IllegalArgumentException");
+                }
+            break;
+            default:
+            break;
+        }
+    }
 }
