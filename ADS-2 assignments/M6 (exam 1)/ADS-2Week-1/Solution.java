4b126dfec3a398386195c720fff89c7cdf80a5e3
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-11-04 16:03:49 +0530

    "ads2_m6_a1_1: 4/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/M6 (exam 1)/ADS-2Week-1/Solution.java
+++ ADS-2 assignments/M6 (exam 1)/ADS-2Week-1/Solution.java
@@ -2,6 +2,7 @@
  * Page Rank.
  */
 import java.util.Scanner;
+import java.util.Arrays;
 /**
  * Class for page rank.
  */
@@ -13,7 +14,7 @@ class PageRank {
     /**
      * declaration of variable.
      */
-    private double value;
+    private int value;
     /**
      * declaration of variable.
      */
@@ -27,19 +28,18 @@ class PageRank {
         this.graph = graphh;
         value = (graph.V());
         reverse = graph.reverse();
-        System.out.println(graph);
     }
-    /**
-     * Gets the pr.
-     *
-     * @param      vertex  The vertex
-     *
-     * @return     The pr.
-     */
-    public double getPR(final int vertex) {
-        double pr = 1.0 / value;
-        return PrCal(pr, vertex);
-    }
+    // /**
+    //  * Gets the pr.
+    //  *
+    //  * @param      vertex  The vertex
+    //  *
+    //  * @return     The pr.
+    //  */
+    // public double getPR(final int vertex) {
+    //     double pr = 1.0 / value;
+    //     return PrCal(pr, vertex);
+    // }
     /**
      * calculation of pr.
      *
@@ -48,29 +48,29 @@ class PageRank {
      *
      * @return     { description_of_the_return_value }
      */
-    public double PrCal(final double pr, final int vertex) {
-        double[] prarr = new double[graph.V()];
-        for (int i = 0; i < prarr.length; i++) {
-            prarr[i] = pr;
+    public void PrCal() {
+        double[] prarr = new double[value];
+        double[] prarr1 = new double[value];
+        for (int i = 0; i < value; i++) {
+            prarr[i] = 1.0 / value;
         }
-        double[] prarr1 = new double[graph.V()];
-        for (int i = 0; i < prarr.length; i++) {
-            prarr1[i] = prarr[i];
-        }
-        for (int iter = 0; iter < 1000; iter++) {
-            for (int i = 0; i < prarr.length; i++) {
-                prarr[i] = prarr1[i];
-            }
-            for (int j = 0; j < graph.V(); j++) {
-                Iterable<Integer> it = reverse.adj(j);
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < value; j++) {
                 double rank = 0.0;
-                for (Integer i : it) {
-                    rank += prarr[i] / graph.outdegree(i);
+                for (int k : reverse.adj(j)) {
+                    rank += prarr[k] / (double) (graph.outdegree(k));
                 }
                 prarr1[j] = rank;
             }
+            if (Arrays.equals(prarr, prarr1)) {
+                break;
+            } else {
+                prarr = prarr1.clone();
+            }
         }
-        return prarr1[vertex];
+        for (int i = 0; i < value; i++) {
+            System.out.print(i + " - " + prarr1[i] + "\n");
+        }
     }
 }
 /**
@@ -103,26 +103,28 @@ public final class Solution {
         // and build the graph
         scan.nextLine();
 
+        Digraph d4PR = new Digraph(vertices);
         Digraph d = new Digraph(vertices);
         for (int i = 0; i < vertices; i++) {
             String[] tokens = scan.nextLine().split(" ");
             if (tokens.length == 1) {
                 for (int j = 0; j < vertices; j++) {
-                    d.addEdge(i, j);
+                    if (j != i) d4PR.addEdge(i, j);
                 }
             } else {
                 for (int j = 1; j < tokens.length; j++) {
                     d.addEdge(Integer.parseInt(tokens[0]),
-                        Integer.parseInt(tokens[j]));
+                              Integer.parseInt(tokens[j]));
+                    d4PR.addEdge(Integer.parseInt(tokens[0]),
+                                 Integer.parseInt(tokens[j]));
                 }
             }
         }
+        System.out.println(d);
         // Create page rank object and pass the graph object to the constructor
         // print the page rank object
-        PageRank p = new PageRank(d);
-        for (int i = 0; i < vertices; i++) {
-            System.out.println(i + " - " + p.getPR(i));
-        }
+        PageRank p = new PageRank(d4PR);
+        p.PrCal();
         // This part is only for the final test case
 
         // File path to the web content
