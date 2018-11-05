e130d3940adbc1443a45fa3b3f856a620eb96848
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-11-05 18:09:13 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/M7/1. MST/LazyPrimMST.java
+++ ADS-2 assignments/M7/1. MST/LazyPrimMST.java
@@ -1,165 +1,99 @@
 /**
  * Class for lazy primitive mst.
  */
-class LazyPrimMST {
+public class LazyPrimMST {
     /**
-     * { var_description }.
+     * {total weight of MST}.
      */
-    private static final double FLOATING_POINT_EPSILON = 1E-12;
+    private double weight;
     /**
-     * { var_description }.
+     * {edges in the MST}.
      */
-    private double weight;       // total weight of MST
+    private Queue<Edge> mst;
     /**
-     * { var_description }.
+     * {marked[v] = true iff v on tree}.
      */
-    private Queue<Edge> mst;     // edges in the MST
+    private boolean[] marked;
     /**
-     * { var_description }.
+     * {edges with one endpoint in tree}.
+     *
      */
-    private boolean[] marked;    // marked[v] = true iff v on tree
-    /**
-     * { var_description }.
-     */
-    private MinPQ<Edge> pq;      // edges with one endpoint in tree
+    private MinPQ<Edge> pq;
 
     /**
-     * Compute a minimum spanning tree (or forest) of an edge-weighted graph.
-     * @param ewg the edge-weighted graph
+     * Compute a minimum spanning tree (or forest).
+     * of an edge-weighted graph.
+     * @param g the edge-weighted graph
      */
-    public LazyPrimMST(final EdgeWeightedGraph ewg) {
+    LazyPrimMST(final EdgeWeightedGraph g) {
         mst = new Queue<Edge>();
         pq = new MinPQ<Edge>();
-        marked = new boolean[ewg.ver()];
-        for (int v = 0; v < ewg.ver(); v++)
-        // run Prim from all vertices to
-            if (!marked[v]) prim(ewg, v);
-            // get a minimum spanning forest
-
-        // check optimality conditions
-        assert check(ewg);
-    }
-
-    // run Prim's algorithm
-    private void prim(final EdgeWeightedGraph ewg, final int s) {
-        scan(ewg, s);
-        while (!pq.isEmpty()) {
-        // better to stop when mst has V-1 edges
-            Edge e = pq.delMin();
-            // smallest edge on pq
-            int v = e.either(), w = e.other(v);
-            // two endpoints
-            assert marked[v] || marked[w];
-            if (marked[v] && marked[w]) continue;
-            // lazy, both v and w already scanned
-            mst.enqueue(e);
-            // add e to MST
-            weight += e.weight();
-            if (!marked[v]) scan(ewg, v);
-            // v becomes part of tree
-            if (!marked[w]) scan(ewg, w);
-            // w becomes part of tree
+        marked = new boolean[g.ver()];
+        for (int v = 0; v < g.ver(); v++) {
+            if (!marked[v]) {
+                prim(g, v);
+            }
         }
     }
 
     /**
-     * { function_description }.
+     * {Private Accessor method of prim}.
      *
-     * @param      ewg   The ewg
-     * @param      v     { parameter_description }
+     * @param      g     {EdgeWeightedGraph}
+     * @param      s     {Vertex}
      */
-    private void scan(final EdgeWeightedGraph ewg, final int v) {
-        assert !marked[v];
+    private void prim(final EdgeWeightedGraph g,
+                      final int s) {
+        scan(g, s);
+        while (!pq.isEmpty()) {
+            Edge e = pq.delMin();
+            int v = e.either();
+            int w = e.other(v);
+            if (marked[v] && marked[w]) {
+                continue;
+            }
+            mst.enqueue(e);
+            weight += e.weight();
+            if (!marked[v]) {
+                scan(g, v);
+            }
+            if (!marked[w]) {
+                scan(g, w);
+            }
+        }
+    }
+
+    /**
+     * {add all edges e incident to v onto pq if the.
+     * other endpoint has not yet been scanned}
+     *
+     * @param      g     {EdgeWeightedGraph}
+     * @param      v     {Vertex}
+     */
+    private void scan(final EdgeWeightedGraph g,
+                      final int v) {
         marked[v] = true;
-        for (Edge e : ewg.adj(v))
-            if (!marked[e.other(v)]) pq.insert(e);
+        for (Edge e : g.adj(v)) {
+            if (!marked[e.other(v)]) {
+                pq.insert(e);
+            }
+        }
     }
 
     /**
      * Returns the edges in a minimum spanning tree (or forest).
-     * @return the edges in a minimum spanning tree (or forest) as
-     *    an iterable of edges
+     * @return the edges in a minimum spanning tree (or forest)
+     *  as an iterable of edges
      */
-    public Iterable<Edge> edges() {
+    public Queue<Edge> edges() {
         return mst;
     }
 
     /**
-     * Returns the sum of the edge weights in a minimum spanning tree.
-     * (or forest).
-     * @return the sum of the edge weights in a minimum spanning tree
-     * (or forest)
+     * @return the sum of the edge weights.
+     * in a minimum spanning tree (or forest)
      */
     public double weight() {
         return weight;
-    }
-
-    /**
-     * { function_description }.
-     *
-     * @param      ewg   The ewg
-     *
-     * @return     { description_of_the_return_value }
-     */
-    private boolean check(final EdgeWeightedGraph ewg) {
-
-        // check weight
-        double totalWeight = 0.0;
-        for (Edge e : edges()) {
-            totalWeight += e.weight();
-        }
-        if (Math.abs(totalWeight - weight()) > FLOATING_POINT_EPSILON) {
-            System.err.printf(
-                "Weight of edges does not equal weight(): %f vs. %f\n",
-                totalWeight, weight());
-            return false;
-        }
-
-        // check that it is acyclic
-        UF uf = new UF(ewg.ver());
-        for (Edge e : edges()) {
-            int v = e.either(), w = e.other(v);
-            if (uf.connected(v, w)) {
-                System.err.println("Not a forest");
-                return false;
-            }
-            uf.union(v, w);
-        }
-
-        // check that it is a spanning forest
-        for (Edge e : ewg.edges()) {
-            int v = e.either(), w = e.other(v);
-            if (!uf.connected(v, w)) {
-                System.err.println("Not a spanning forest");
-                return false;
-            }
-        }
-
-        // check that it is a minimal spanning forest
-        //(cut optimality conditions)
-        for (Edge e : edges()) {
-
-            // all edges in MST except e
-            uf = new UF(ewg.ver());
-            for (Edge f : mst) {
-                int x = f.either(), y = f.other(x);
-                if (f != e) uf.union(x, y);
-            }
-
-            // check that e is min weight edge in crossing cut
-            for (Edge f : ewg.edges()) {
-                int x = f.either(), y = f.other(x);
-                if (!uf.connected(x, y)) {
-                    if (f.weight() < e.weight()) {
-                        System.err.println("Edge " + f +
-                                           " violates cut optimality conditions");
-                        return false;
-                    }
-                }
-            }
-
-        }
-
-        return true;
     }
 }
