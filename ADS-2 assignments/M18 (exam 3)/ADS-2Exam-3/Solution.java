081404954c032ee7edddf1ecd2f1534ff572f6f6
swapnika-20186045 <vakacharlaswapnika@msitprogram.net>
2018-11-17 11:52:25 +0530

    "ads2_m18_a1_1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/M18 (exam 3)/ADS-2Exam-3/Solution.java
+++ ADS-2 assignments/M18 (exam 3)/ADS-2Exam-3/Solution.java
@@ -146,11 +146,46 @@ class T9 {
 
 	public Iterable<String> potentialWords(final String t9Signature) {
 		// your code goes here
-		return null;
+		ArrayList<String> arrlist = new ArrayList<>();
+		for (String each : tst.keys()) {
+			String[] strarr = each.split(" ");
+			String number = "";
+			for (String ch : strarr) {
+				if(ch.equals("a") || ch.equals("b") || ch.equals("c")) {
+					number = number + "2";
+				}
+				if(ch.equals("d") || ch.equals("e") || ch.equals("f")) {
+					number = number + "3";
+				}
+				if(ch.equals("g") || ch.equals("h") || ch.equals("i")) {
+					number = number + "4";
+				}
+				if(ch.equals("j") || ch.equals("k") || ch.equals("l")) {
+					number = number + "5";
+				}
+				if(ch.equals("m") || ch.equals("n") || ch.equals("o")) {
+					number = number + "6";
+				}
+				if(ch.equals("p") || ch.equals("q") || ch.equals("r") || ch.equals("s")) {
+					number = number + "7";
+				}
+				if(ch.equals("t") || ch.equals("u") || ch.equals("v")) {
+					number = number + "8";
+				}
+				if(ch.equals("w") || ch.equals("x") || ch.equals("y") || ch.equals("z")) {
+					number = number + "9";
+				}
+			}
+			if (number.equals(t9Signature)) {
+				arrlist.add(each);
+			}
+		}
+		return arrlist;
 	}
 
 	// return all possibilities(words), find top k with highest frequency.
-	public Iterable<String> getSuggestions(final Iterable<String> words, final int k) {
+	public Iterable<String> getSuggestions(final Iterable<String> words,
+	                                       final int k) {
 		// your code goes here
 		ArrayList<String> arr = new ArrayList<>();
 		MaxPQ<Integer> max = new MaxPQ<>();
@@ -171,7 +206,7 @@ class T9 {
 
 	// final output
 	// Don't modify this method.
-	public Iterable<String> t9(String t9Signature, int k) {
+	public Iterable<String> t9(final String t9Signature, final int k) {
 		return getSuggestions(potentialWords(t9Signature), k);
 	}
 }
